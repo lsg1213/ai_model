f761f3bcc6c55ef3a2baa2c991be51f57ee5f9a7
Seungjin Lee <lsg0385@naver.com>
2020-09-20 14:37:30 +0900

    [add] eval.py success

---
+++ pytorch/test_model/README.md
@@ -0,0 +1,8 @@
+eval directory architecture
+. -- eval.py
+  -- eval_utils.py
+  -- dataset -- stationary_accel_data.pickle
+             -- stationary_sound_data.pickle
+             -- transfer_f.pickle
+  -- model_save -- *.pt
+  
--- pytorch/test_model/eval_utils.py
+++ pytorch/test_model/eval_utils.py
@@ -1,6 +1,9 @@
 import torch, torchaudio, pdb
 from torch.utils.data import DataLoader, Dataset
 import numpy as np
+import scipy
+from scipy.signal import welch, hann
+import matplotlib.pyplot as plt
 import torch.nn.functional as F
 from scipy.io.wavfile import write
 
@@ -77,3 +80,78 @@ def conv_with_S(signal, S_data, config, device=torch.device('cpu')):
     out = F.conv1d(signal, S_data.permute([2,0,1]).type(signal.dtype)).transpose(1,2)[:,:-1,:]
     
     return out 
+
+
+def filter_A(F):
+    c1 = 3.5041384e16
+    c2 = 20.598997 ** 2
+    c3 = 107.65265 ** 2
+    c4 = 737.86223 ** 2
+    c5 = 12194.217 ** 2
+
+    f = F
+    arr = np.array(list(map(lambda x : 1e-17 if x == 0 else x, f)))
+
+    num = np.sqrt(c1 * (arr ** 8))
+    den = (f ** 2 + c2) * np.sqrt((f ** 2 + c3) * (f**2 + c4)) * (f ** 2 + c5)
+    A = num / den
+    return A
+def dBA_metric(y, gt, plot=True):
+    """
+    |args|
+    :y: generated sound data, it's shape should be (time, 8)
+    :gt: ground truth data, it's shape should be (time, 8)
+    :plot: if True, plot graph of each channels
+    """
+    d = gt
+    e = gt - y
+    Tn = y.shape[0]
+    K = 8
+    M = 8
+    """Post processing : performance metric and plots"""
+    p_ref = 20e-6
+    fs = 2000
+    Nfft = fs
+    noverlap = Nfft / 2
+    t = (np.arange(Tn) / fs)[np.newaxis, :]
+    win = hann(fs, False)
+    #autopower calculation
+    D = np.zeros((int(Nfft/2 + 1), M))
+    E = np.zeros((int(Nfft/2 + 1), M))
+    for m in range(M):
+        F, D[:,m] = welch(d[:, m], fs, window=win, noverlap=noverlap, nfft=Nfft, return_onesided=True, detrend=False)
+        F, E[:,m] = welch(e[:, m], fs, window=win, noverlap=noverlap, nfft=Nfft, return_onesided=True, detrend=False)
+    
+    A = filter_A(F)
+    AA = np.concatenate([[A]] * M, axis=0).transpose(1,0)
+    D_A = D * AA ** 2 / p_ref ** 2
+    E_A = E * AA ** 2 / p_ref ** 2
+    
+    # perfomance metric calculation
+    D_A_dBA_Sum = np.zeros((1,M))
+    E_A_dBA_Sum = np.zeros((1,M))
+    freq_range = np.arange(500)
+    result = []
+    for m in range(M):
+        D_A_dBA_Sum[0, m] = 10 * np.log10(np.sum(D_A[freq_range, m]))
+        E_A_dBA_Sum[0, m] = 10 * np.log10(np.sum(E_A[freq_range, m]))
+        result.append(D_A_dBA_Sum[0,m] - E_A_dBA_Sum[0,m])
+    result.append(np.array(np.mean(result)))
+    avg_result = np.mean(result)
+    
+    if plot:
+        for m in range(M):
+            plt.subplot(2, 4, m+1)
+            da = D_A[:,m]
+            ea = E_A[:,m]
+            plt.plot(F, 10 * np.log10(da), color="red", label=f'D{D_A_dBA_Sum[0, m]:.2f}dBA')
+            plt.plot(F, 10 * np.log10(ea), color="blue", label=f'E{E_A_dBA_Sum[0, m]:.2f}dBA')
+            plt.legend()
+            plt.ylim((10, 60))
+            plt.xlim((20,1000))
+            plt.xlabel('Frequency(Hz)')
+            plt.ylabel('Level(dBA)')
+        plt.tight_layout()
+        plt.show()
+    
+    return avg_result
